fc823cb6c6cf2347b552fefebd9f756f157ee912
Ken Snyder <kendsnyder@gmail.com>
2016-07-15 09:00:07 -0600

    [master] unit test for parseplus.clearParsers()

--- test/extending.spec.js
+++ test/extending.spec.js
@@ -3,7 +3,7 @@ var parseplus = require('../parseplus.js');
 var expect = require("chai").expect;
 
 describe("extending parseplus", function() {
-	
+
 	it("should add handler", function () {
 		parseplus.addParser({
 			name: 'test2',
@@ -26,6 +26,21 @@ describe("extending parseplus", function() {
 		expect(moment('test3 12').valueOf()).to.equal(+new Date(1970,0,12));
 		parseplus.removeParser('test3');
 		expect(moment('test3 12').isValid()).to.equal(false);
+	});
+	it("should clear out parsers", function () {
+		parseplus.addParser({
+			name: 'test4',
+			matcher: /^test4 (\d+)$/,
+			handler: function(match) {
+				return new Date(1970,0,+match[1]);
+			}
+		});
+		expect(moment('test4 12').valueOf()).to.equal(+new Date(1970,0,12));
+		var parsers = parseplus.parsers;
+		parseplus.clearParsers();
+		expect(moment('test4 12').isValid()).to.equal(false);
+		parseplus.parsers = parsers;
+		parseplus.removeParser('test4');
 	});
 
 });
