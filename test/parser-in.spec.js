d15ae6541ca6cb4b86b88cd4a796a4edc55feedb
Ken Snyder <kendsnyder@gmail.com>
2016-07-20 14:52:39 -0600

    [master] rename some parsers

--- test/parser-in.spec.js
+++ test/parser-in.spec.js
@@ -2,12 +2,12 @@ var moment = require('moment');
 require('../parseplus.js');
 var expect = require("chai").expect;
 
-describe("in time parser", function() {
+describe("in parser", function() {
 
 	it("should parse dates times like `in 5 days`", function () {
 		expect(+moment('in 5 days')).to.be.closeTo(+moment().add(5, 'days'), 1000);
 	});
-	it("should parse dates times like `5.5 months ago`", function () {
+	it("should parse dates times like `in 5.5 months`", function () {
 		expect(+moment('in 5.5 months')).to.be.closeTo(+moment().add(5.5, 'months'), 1000);
 	});
 
